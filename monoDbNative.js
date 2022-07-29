294341940117be8440b1ebeb3276ba6d423d6bc8
surajarya130 <surajarya130@gmail.com>
2022-07-29 19:05:20 +0530

    #3  commit 29th Jul

--- monoDbNative.js
+++ monoDbNative.js
@@ -1,5 +1,4 @@
 const {MongoClient} = require("mongodb");
-// const url = "mongodb+srv://root:suraj123@cluster0.dyiou.mongodb.net/puppySpa?retryWrites=true&w=majority";
 const client =  new MongoClient(process.env.MONGO_URL);
 const databaseName = 'puppySpa';
 
