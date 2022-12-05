87f9cace36e9a50d3e3a84c2378fa95b82114f2c
surajarya130 <surajarya130@gmail.com>
2022-12-05 20:07:06 +0530

    #4 5th Dec

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 // Defining all the required package below
-require('dotenv').config();
-const express = require("express")
-const EventEmitter = require("events")
+require("dotenv").config();
+const express = require("express");
+const EventEmitter = require("events");
 const app = express();
 const port = process.env.PORT || 3000;
-let dogsRouter = require("./routes/dogs")
-let cors = require("cors")
+let dogsRouter = require("./routes/dogs");
+let cors = require("cors");
 let dbConnect = require("./mongodb.js");
-const natDbCon = require('./monoDbNative');
-const emitter = new EventEmitter()
-emitter.setMaxListeners(100)
-app.use(express.json())
-app.use(cors())
+const natDbCon = require("./monoDbNative");
+const emitter = new EventEmitter();
+emitter.setMaxListeners(100);
+app.use(express.json());
+app.use(cors());
 
-
-// Home Page 
-app.get("/", async(req, res)=>{
-    dbConnect()
-    res.send("Worked home page")
-})
+// Home Page
+app.get("/", async (req, res) => {
+  dbConnect();
+  res.send("Worked home page");
+});
 
 // Defining the route of pups page
-app.use('/dogs', dogsRouter)
+app.use("/dogs", dogsRouter);
 
-app.listen(port, ()=>{
-    console.log(`Listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
--- routes/dogs.js
+++ routes/dogs.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
 let express = require("express");
 let router = express.Router();
 let Puppys = require("../schemas/puppiesSchema.js");
-let PuppyShcema = require("../schemas/PuppyShcema.js")
+let PuppyShcema = require("../schemas/PuppyShcema.js");
 const dbConnect = require("../mongodb.js");
-const natDbCon = require('../monoDbNative.js');
-
+const natDbCon = require("../monoDbNative.js");
 
 // Checkout dog serviced pups on current date
 router.patch("/checkout/:dogName", (req, res) => {
@@ -13,10 +12,10 @@ router.patch("/checkout/:dogName", (req, res) => {
   let checkedPup = PuppyShcema.findOneAndUpdate(
     { Name: req.params.dogName },
     { OutStatus: true },
-    {new: true},
-    (err, result)=>{
-      if(err) throw err;
-      res.send(result)
+    { new: true },
+    (err, result) => {
+      if (err) throw err;
+      res.send(result);
       // Below code to remove from the current collection and store to another colleciton can be useful further
       // else{
       //   let movePupTo = PuppyShcema.insertMany({
@@ -27,84 +26,82 @@ router.patch("/checkout/:dogName", (req, res) => {
       //   })
       // }
     }
-  );  
+  );
 });
 
-
 // Sorting the pups list in descending order
-router.get("/sortedlist", async(req, res) => {
+router.get("/sortedlist", async (req, res) => {
   let data = Puppys.find({})
     .sort({ createdAt: -1 })
     .exec(function (err, docs) {
       if (err) throw err;
       res.send(docs);
     });
-
 });
-
 
 // Adding a pup api
 router.post("/addPuppy", async (req, res) => {
   dbConnect();
-  let Pup = new PuppyShcema(req.body)
+  let Pup = new PuppyShcema(req.body);
   try {
     await Pup.save();
     // console.log("Puppy Added");
-    let saveToMainRecord = new Puppys(req.body)
+    let saveToMainRecord = new Puppys(req.body);
     saveToMainRecord.save();
     res.send(Pup);
   } catch (error) {
     res.status(500).send(error);
   }
-
 });
 
 // To fetch datewise collection
-router.get("/datewise/:date", async(req, res)=>{
+router.get("/datewise/:date", async (req, res) => {
   const puppsCollection = await natDbCon(req.params.date);
   const allPups = await puppsCollection.find({}).toArray();
-  res.send(allPups)
-
-})
-
+  res.send(allPups);
+});
 
 // Live search by name router
-router.get("/livesearch/:liveupname", async(req, res)=>{
+router.get("/livesearch/:liveupname", async (req, res) => {
   const db = await natDbCon("puppiesdetails");
-  const dataByName = await db.find({ Name: {$regex: req.params.liveupname, $options: 'i'}}).toArray();
+  const dataByName = await db
+    .find({ Name: { $regex: req.params.liveupname, $options: "i" } })
+    .toArray();
   if (dataByName.length === 0) {
     return res.send([]);
   } else {
     return res.send(dataByName);
   }
-})
+});
 
-// No of total pups registered till date 
-router.get("/totalPups", (req, res)=>{
+// No of total pups registered till date
+router.get("/totalPups", (req, res) => {
   dbConnect();
-  let data = Puppys.find({}, (err, result)=>{
-    if(err) throw err;
+  let data = Puppys.find({}, (err, result) => {
+    if (err) throw err;
     res.send(result);
-  })
-})
+  });
+});
 
 // Search by Puppy Name from main db
-router.get("/:pupname", (req, res)=>{
+router.get("/:pupname", (req, res) => {
   dbConnect();
-  let data = Puppys.find({Name: req.params.pupname}, (err, result)=>{
-    if(err) throw err;
+  let data = Puppys.find({ Name: req.params.pupname }, (err, result) => {
+    if (err) throw err;
     res.send(result);
-  })
-})
+  });
+});
 
 // Get current pups which have not been served off
 router.get("/", (req, res) => {
   dbConnect();
-  let allPups = PuppyShcema.find({OutStatus: {$ne: true}}, (err, puppyList) => {
-    if (err) throw err;
-    res.send(puppyList);
-  });
+  let allPups = PuppyShcema.find(
+    { OutStatus: { $ne: true } },
+    (err, puppyList) => {
+      if (err) throw err;
+      res.send(puppyList);
+    }
+  );
 });
-
 
 module.exports = router;
